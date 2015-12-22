ac1cd351487714d28778b1fd980cd72bb9822733
margin <990768144@qq.com>
2015-12-22 20:56:04 +0800

    修改编码，使其能在命令行中执行。

--- src/analysis/crawlerScript.py
+++ src/analysis/crawlerScript.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*-
+import Queue,time
 from scrapy.crawler import CrawlerProcess
-from tutorial.spiders.marginer_spider import FollowAllSpider
-
-def spider_working(url='https://zh.wikipedia.org/wiki/%E5%9C%9F%E8%B1%86%E7%BD%91'):
-    spider = FollowAllSpider(domain=url)
-    process = CrawlerProcess({
-        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-    })
-
-    process.crawl(spider)
-    # 线程阻塞，爬完之后自己结束线程。
-    process.start()
+from study.tutorial.spiders.marginer_spider import FollowAllSpider
+from queue import *
+def spider_working():
+    while True:
+        time.sleep(1.5)
+        print 111111111111111
+        print spider_queue.qsize() 
+        if spider_queue.qsize():
+            url = spider_queue.get(1)
+            print 22222222222222
+            print url 
+            spider = FollowAllSpider(domain=url)
+            process = CrawlerProcess({
+                'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+            })
+            
+            process.crawl(spider)
+            # 线程阻塞，爬完之后自己结束线程。
+            process.start()
--- src/marginer.py
+++ src/marginer.py
@@ -6,12 +6,19 @@ from recognition.translator import voice2words
 import traceback,sys,Queue,os
 from network.baiduApi import BaiduApi
 from margin.speeker import Speeker
+from analysis.crawlerScript import spider_working
+from queue import *
+from multiprocessing import Process
 
 try:
-    
+    print '=============='
+    #spider_queue.put("https://zh.wikipedia.org/wiki/%E8%8B%B1%E9%9B%84%E8%81%94%E7%9B%9F")
+    print spider_queue.qsize()
+    # 爬虫队列
+    #spider_queue=Queue.Queue(1024)
     speek = Speeker()
     # 语音反馈
-    speek.speek({},"margin,晚上好。")
+    speek.speek({},u"margin,晚上好。")
     # 获取百度语音的token
     baiduApi = BaiduApi()
     baiduApi.init_token()
@@ -20,7 +27,9 @@ try:
     wav_queue=Queue.Queue(1024)
     # 开始工作录音
     recorder.start(wav_queue)
+    #spider_working()
     # 开始音频解析
     voice2words.start(wav_queue,token)
+    
 except:
     traceback.print_exc(file=sys.stdout)
