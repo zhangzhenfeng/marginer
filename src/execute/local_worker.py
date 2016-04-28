bde5f3b0bd656da7f98504a8a40ad2e8e05f4492
margin <990768144@qq.com>
2016-04-28 21:58:29 +0800

    .

--- src/execute/local_worker.py
+++ src/execute/local_worker.py
@@ -5,12 +5,14 @@
 """
 from worker.music.get_xiami_music import get_music_from_xiami
 from margin.speeker import Speeker
+import os
 class LocalWorker():
     def __init__(self):
         self.speeker = Speeker()
     def handle(self,scener = {}):
         # scene为场景元素
         if scener.get("code") == "音乐":
-            get_music_from_xiami()
+            #get_music_from_xiami()
+            os.system("python get_xiami_music.py")
             self.speeker.speek({},u"正在为您下载虾米音乐top10")
         
