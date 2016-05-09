93eccefc9cf428b989ca1f6fd56f817e676cf511
margin <990768144@qq.com>
2016-05-09 22:44:11 +0800

    .

--- src/execute/local_worker.py
+++ src/execute/local_worker.py
@@ -3,16 +3,21 @@
     处理本地任务
     例如播放音乐，提示室内温湿度，提示今天天气状况
 """
-from worker.music.get_xiami_music import get_music_from_xiami
+from worker.music.get_xiami_music import DownLoadMusic,PlayMusic
 from margin.speeker import Speeker
-import os
+import os,subprocess,time
+import multiprocessing
 class LocalWorker():
     def __init__(self):
         self.speeker = Speeker()
+    
     def handle(self,scener = {}):
         # scene为场景元素
         if scener.get("code") == "音乐":
-            #get_music_from_xiami()
-            os.system("python get_xiami_music.py")
-            self.speeker.speek({},u"正在为您下载虾米音乐top10")
-        
+            #player = DownLoadMusic()
+            #player.setDaemon(True)
+            #player.start()
+            #player.join()
+            #self.speeker.speek({},u"正在为您下载虾米音乐top10")
+            p=subprocess.Popen("python worker/music/playMusic.py", shell=True) 
+            p.wait()
